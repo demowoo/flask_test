6696a6a1095d8eb8204d084f6e5e979f06481d6f
吴瑞平 <wuruiping@kugou.net>
2015-09-02 18:03:54 +0800

    添加配置

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+#coding:utf-8
 __author__ = 'demowu'
+
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+if __name__ == '__main__':
+    print app.config['MAIL_FROM_EMAIL']
+    app.run()
--- config.py
+++ config.py
@@ -1,1 +1,6 @@
+#coding:utf-8
 __author__ = 'demowu'
+
+DEBUG = True # 启动Flask的Debug模式
+BCRYPT_LEVEL = 13 # 配置Flask-Bcrypt拓展
+MAIL_FROM_EMAIL = "robert@example.com" # 设置邮件来源
